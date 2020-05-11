333597d8a70a1229e6c99ee0c92868da40fff60f
swati pandey <sp134363@gmail.com>
2020-05-11 23:47:31 +0530

    added pycharm gitignore

--- bshare/urls.py
+++ bshare/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path ,include
-
+import  home.views as home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('',home.home_view,name='home'),]

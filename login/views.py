d31ce74e4f0e9e350686b20402643c14c3cf9b1d
swati pandey <sp134363@gmail.com>
2020-05-25 19:56:39 +0530

    new commit

--- login/views.py
+++ login/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
-from .import templates
+
 # Create your views here.
 def loginsignup(request):
     msg =""
